875e63906a210e09c3e0687bc24e3e3f75666eea
otsubyo <carlos.premi@gmail.com>
2025-06-10 11:50:45 +0200

    update emoji

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.use('/api/lines', lineRoutes); // line routes have auth inside
 app.use('/api/stats', statsRoutes); // stats routes have auth inside
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
