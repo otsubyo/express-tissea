55c4c47f9b507fa14a981fc73d9a82ae266116d9
otsu <you@example.com>
2025-06-18 05:32:25 +0200

    index

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,5 +2,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Home = () => <h1>Bienvenue sur TissÉa</h1>;
+const Home = () => <h1>Bienvenue sur Tisséa</h1>;
 export default Home;
